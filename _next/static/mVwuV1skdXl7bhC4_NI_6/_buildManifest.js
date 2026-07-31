70e18ddc4a8f1b0ef4963155e64367bf3a50db8b
shankarpandala <shankarpandala@users.noreply.github.com>
2026-07-31 09:37:30 +0000

    deploy: b96f76234b92dad504096bd5d2a47a54770045b0

---
+++ _next/static/mVwuV1skdXl7bhC4_NI_6/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/physics4all//_next/:path+",
+        "destination": "/physics4all/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
